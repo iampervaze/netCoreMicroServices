428301ea25e5864b5b2ec022c674142702ae1219
Pervaze Ahanger <pahanger@nthrive.com>
2018-05-11 23:39:56 -0600

    Add Unit and Integration Tests for Activities API's

---
+++ Action.Services.Activities.Tests/Integration/Controllers/HomeControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+
+namespace Action.Services.Activities.Tests.Integration.Controllers
+{
+    public class HomeControllerTests
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public HomeControllerTests()
+        {
+            var builder = WebHost.CreateDefaultBuilder()
+                .UseStartup<Startup>()
+                .UseEnvironment("Development");
+            _server = new TestServer(builder);
+            _client = _server.CreateClient();
+        }
+
+        [Fact]
+        public async Task home_controller_get_should_return_string_content()
+        {
+            var response = await _client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            content.Should().BeEquivalentTo("Hello From Action API");
+        }
+    }
+}
---
+++ Action.Services.Activities.Tests/Unit/Services/ActivityServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Action.Services.Activities.Domain.Models;
+using Action.Services.Activities.Domain.Repositories;
+using Action.Services.Activities.Services;
+using Moq;
+using Xunit;
+
+namespace Action.Services.Activities.Tests.Unit.Services
+{
+    public class ActivityServiceTests
+    {
+        [Fact]
+        public async Task activity_service_add_async_should_succeed()
+        {
+            var activityRepositoryMocked = new Mock<IActivityRepository>();
+            var categoryRepositoryMocked = new Mock<ICategoryRepository>();
+
+            var category = "test";
+
+            categoryRepositoryMocked.Setup(x => x.GetAsync(category)).ReturnsAsync(new Category(category));
+
+            var activityService = new ActivityService(activityRepositoryMocked.Object, categoryRepositoryMocked.Object);
+
+            var id = Guid.NewGuid();
+            await activityService.AddAsync(id, Guid.NewGuid(), category, "name", "desc", DateTime.Now);
+
+            categoryRepositoryMocked.Verify(x => x.GetAsync(category), Times.Once);
+
+            activityRepositoryMocked.Verify(x => x.AddAsync(It.IsAny<Activity>()), Times.Once);
+        }
+    }
+}
